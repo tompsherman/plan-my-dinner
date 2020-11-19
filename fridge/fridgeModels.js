5264474bbbce7eb19fa397c7775e3f77308bc311
unknown <tompsherman@gmail.com>
2020-11-18 21:21:10 -0500

    working on fridge crud

--- fridge/fridgeModels.js
+++ fridge/fridgeModels.js
@@ -3,15 +3,36 @@ const db = require('../data/db-config')
 module.exports = {
     //function names:
     find,
+    findBy,
+    findById,
     create,
+    update,
+    throwOut
 }
 
     //functions:
     function find(){
         return db('fridge')
     }
+
+    function findBy(filter){
+        return db('fridge').where(filter).first()
+    }
+
+    function findById(id){
+        return db('fridge').where({id}).first()
+    }
+
     async function create(food){
         const [id] = await
         db('fridge').insert(food)
             return db('fridge').where({id}).first()
+    }
+
+    async function update(id){
+        return db('fridge').where({id}).first()
+    }
+
+    async function throwOut(id){
+        return db('fridge').where({id}).first()
     }
